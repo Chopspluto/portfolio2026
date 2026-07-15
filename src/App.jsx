8ee5c7c1bf9ce5b9fc0ffc81e33e15c6ce737206
Chopspluto <kanishkajayani2@gmail.com>
2026-07-15 19:39:29 +0530

    Remove extra dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,6 @@
-import { useRef, useState } from 'react';
-import { Routes, Route, useLocation, Outlet } from 'react-router-dom';
 import Home from './components/Home';
-import { FiLinkedin } from "react-icons/fi";
-import { FaGithubAlt } from "react-icons/fa6";
-import { MdAlternateEmail } from "react-icons/md";
-import { FiCodepen } from "react-icons/fi";
-import { AnimatePresence } from 'framer-motion';
 
 function App() {
-
-  const location = useLocation();
 
   return (
     <>
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-// import './index.css'
 import './styles/global.css';
 import './styles/home.css';
 import './styles/responsive.css';
@@ -9,13 +7,6 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/portfolio2026">
-      {/* <App /> */}
-      <Routes>
-        <Route path="/*" element={<App />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </StrictMode>
-
-  // basename="/portfolio2026"
 )
